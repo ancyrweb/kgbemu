17f58e1c93180820a342722b1e4e066e10eb0988
Anthony Cyrille <12291995+ancyrweb@users.noreply.github.com>
2025-12-14 06:28:01 +0100

    task: illegal opcode move

--- src/main/kotlin/gameboyemulator/assembly/OpcodeScanner.kt
+++ src/main/kotlin/gameboyemulator/assembly/OpcodeScanner.kt
@@ -1,14 +1,15 @@
 package fr.ancyrweb.gameboyemulator.assembly
 
 import fr.ancyrweb.gameboyemulator.assembly.opcodes.CallOpcode
+import fr.ancyrweb.gameboyemulator.assembly.opcodes.IllegalOpcode
 import fr.ancyrweb.gameboyemulator.assembly.opcodes.JumpOpcode
 import fr.ancyrweb.gameboyemulator.assembly.opcodes.NopOpcode
 import fr.ancyrweb.gameboyemulator.assembly.opcodes.Opcode
 import fr.ancyrweb.gameboyemulator.assembly.opcodes.UnknownOpcode
 
 /**
- * Scans a byte array and converts it to a list of opcodes.
- * The scanner parses the entire byte array from start to finish.
+ * Scans a byte array and converts it to a list of opcodes. The scanner parses the entire byte array
+ * from start to finish.
  *
  * @param bytes The byte array to scan
  * @param startAddress The memory address where the byte array starts (default: 0)
@@ -21,8 +22,8 @@ class OpcodeScanner(
   private val opcodes: MutableList<Opcode> = mutableListOf()
 
   /**
-   * Scans the byte array and converts bytes to opcodes.
-   * The scanning continues until the end of the byte array.
+   * Scans the byte array and converts bytes to opcodes. The scanning continues until the end of the
+   * byte array.
    */
   fun scan() {
     opcodes.clear()
@@ -37,13 +38,23 @@ class OpcodeScanner(
 
   /**
    * Returns all the opcodes that have been scanned.
-   */
+   * */
   fun all(): List<Opcode> {
     return opcodes.toList()
   }
 
+  /**
+   * Returns the opcode at the given index in the byte array.
+   */
   private fun getOpcodeAt(bytes: ByteArray, index: Int, address: Int): Opcode {
-    return when (bytes[index].toInt() and 0xFF) {
+    val opcodeByte = bytes[index].toInt() and 0xFF
+
+    // Check if this is an illegal opcode first
+    if (IllegalOpcode.isIllegal(opcodeByte)) {
+      return IllegalOpcode.fromBytes(bytes, index, address)
+    }
+
+    return when (opcodeByte) {
       0x00 -> {
         NopOpcode.fromBytes(address)
       }
@@ -62,4 +73,3 @@ class OpcodeScanner(
     }
   }
 }
-
--- src/main/kotlin/gameboyemulator/assembly/opcodes/IllegalOpcode.kt
+++ src/main/kotlin/gameboyemulator/assembly/opcodes/IllegalOpcode.kt
@@ -7,8 +7,17 @@ class IllegalOpcode(opCodeAddress: Int, byteValue: UByte) :
     1,
   ) {
   companion object {
-    fun fromBytes(bytes: ByteArray, index: Int, address: Int): UnknownOpcode {
-      return UnknownOpcode(address, bytes[index].toUByte())
+    /**
+     * Checks if an opcode byte value is illegal (not assigned) in the Game Boy CPU.
+     * According to the Game Boy CPU specification, the following opcodes are illegal:
+     * 0xD3, 0xDB, 0xDD, 0xE3, 0xE4, 0xEB, 0xEC, 0xED, 0xF4, 0xFC, 0xFD
+     */
+    fun isIllegal(opcodeByte: Int): Boolean {
+      return opcodeByte in setOf(0xD3, 0xDB, 0xDD, 0xE3, 0xE4, 0xEB, 0xEC, 0xED, 0xF4, 0xFC, 0xFD)
+    }
+
+    fun fromBytes(bytes: ByteArray, index: Int, address: Int): IllegalOpcode {
+      return IllegalOpcode(address, bytes[index].toUByte())
     }
   }
 }

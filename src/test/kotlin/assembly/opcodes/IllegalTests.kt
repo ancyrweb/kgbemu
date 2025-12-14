0c8390935b2aa54be84a3b767cc4ffb8a11e4d2e
Anthony Cyrille <12291995+ancyrweb@users.noreply.github.com>
2025-12-14 06:41:12 +0100

    chore: better naming of illegal tests

--- src/test/kotlin/assembly/opcodes/IllegalTests.kt
+++ src/test/kotlin/assembly/opcodes/IllegalTests.kt
@@ -6,9 +6,10 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
 class IllegalTests {
-  @ParameterizedTest(name = "opcode 0x{0} should be illegal")
-  @ValueSource(ints = [0xD3, 0xDB, 0xDD, 0xE3, 0xE4, 0xEB, 0xEC, 0xED, 0xF4, 0xFC, 0xFD])
-  fun `test illegal opcodes parsing`(opcodeValue: Int) {
+  @ParameterizedTest(name = "opcode {0} should be illegal")
+  @ValueSource(strings = ["0xD3", "0xDB", "0xDD", "0xE3", "0xE4", "0xEB", "0xEC", "0xED", "0xF4", "0xFC", "0xFD"])
+  fun `test illegal opcodes parsing`(opcodeHex: String) {
+    val opcodeValue = opcodeHex.removePrefix("0x").toInt(16)
     val opcodes =
         OpcodeTestUtils.scan(
             bytes =
@@ -20,6 +21,6 @@ class IllegalTests {
     Assertions.assertEquals(1, opcodes.size)
 
     val opcode = opcodes[0]
-    Assertions.assertTrue(opcode is IllegalOpcode, "Opcode 0x${opcodeValue.toString(16).uppercase()} should be an IllegalOpcode")
+    Assertions.assertTrue(opcode is IllegalOpcode, "Opcode $opcodeHex should be an IllegalOpcode")
   }
 }
